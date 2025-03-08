7d03e8b217bf004da75e2bc70f4a632f2145bb90
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2025-03-08 11:44:51 +0300

    Complete labs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Habiba</h1>
+      <img src="your-image-url.jpg" alt="A picture of Habiba" />
+      <h2>About Me</h2>
+      <p>I am a software developer passionate about building impactful solutions.</p>
+      <a href="https://github.com/yourusername">GitHub</a>
+      <a href="https://linkedin.com/in/yourusername">LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
